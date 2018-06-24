05de31d83e5b799624089e2af520841a196633fb
chxuan <787280310@qq.com>
2018-06-24 22:43:09 +0800

    update

--- include/log_file_proxy.h
+++ include/log_file_proxy.h
@@ -4,14 +4,16 @@
 日期:2018-06-24
 作者:chxuan <787280310@qq.com>
 */
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <atomic>
 #include "utility.h"
 
 class log_file_proxy
 {
 public:
-    ~log_file_proxy() { if (fd_ != -1) { close(fd_); } }
+    ~log_file_proxy() { close_file(fd_); }
 
     // 初始化日志文件
     inline void init_log_file(const std::string& output_dir, const std::string& level, unsigned long long max_file_size);
@@ -19,19 +21,24 @@ public:
     inline void write_log(const std::string& log);
 
 private:
+    // 创建文件
+    inline void create_file();
     // 打开文件
     inline bool open_file(const std::string& time_ms);
     // 创建软链接
     inline void create_link(const std::string& time_ms);
     // 写文件
     inline void write_file(const std::string& log);
+    // 判断文件是否打开
+    bool is_opened() { return fd_ > 0; }
+    // 关闭文件
+    void close_file(int fd) { if (fd > 0) { close(fd); printf("关闭文件成功, fd:%d, 线程id:%lu\n", fd, pthread_self()); } }
 
 private:
     std::string file_name_;
     std::string file_name_with_dir_;
     unsigned long long max_file_size_;
     std::atomic<unsigned long long> curr_file_size_ {0};
-    std::atomic<bool> is_opened_ {false};
     int fd_ = -1;
 };
 
@@ -44,18 +51,47 @@ void log_file_proxy::init_log_file(const std::string& output_dir, const std::str
 
 void log_file_proxy::write_log(const std::string& log)
 {
-    if (is_opened_)
+    if (is_opened())
     {
-        write_file(log);
+        if (curr_file_size_ < max_file_size_)
+        {
+            write_file(log);
+        }
+        else
+        {
+            create_file();
+            if (is_opened())
+            {
+                write_file(log);
+            }
+            else
+            {
+                printf("写日志时候, 遇到没有打开的文件, fd:%d, 线程id:%lu\n", fd_, pthread_self());
+            }
+        }
     }
     else
     {
-        std::string time_ms = get_current_time_ms();
-        if (open_file(time_ms))
+        create_file();
+        if (is_opened())
         {
-            create_link(time_ms);
             write_file(log);
         }
+        else
+        {
+            printf("写日志时候, 遇到没有打开的文件, fd:%d, 线程id:%lu\n", fd_, pthread_self());
+        }
+    }
+}
+
+void log_file_proxy::create_file()
+{
+    int last_fd = fd_;
+    std::string time_ms = get_current_time_ms();
+    if (open_file(time_ms))
+    {
+        close_file(last_fd);
+        create_link(time_ms);
     }
 }
 
@@ -63,14 +99,16 @@ bool log_file_proxy::open_file(const std::string& time_ms)
 {
     std::string file_name = file_name_with_dir_ + "_" + time_ms;
 
-    fd_ = open(file_name.c_str(), O_CREAT | O_RDWR | O_APPEND, 0664);
-    if (fd_ != -1)
+    int fd = open(file_name.c_str(), O_CREAT | O_EXCL | O_RDWR | O_APPEND, 0664);
+    if (fd > 0)
     {
-        is_opened_ = true;
+        fd_ = fd;
+        curr_file_size_ = 0;
+        printf("打开日志文件成功, fd:%d, 线程id:%lu, 文件名:%s\n", fd_, pthread_self(), time_ms.c_str());
         return true;
     }
 
-    printf("写日志时，打开文件失败:%s\n", file_name.c_str());
+    printf("打开日志文件失败, fd:%d, 线程id:%lu, 错误原因:%s, 文件名:%s\n", fd, pthread_self(), strerror(errno), file_name.c_str());
 
     return false;
 }
@@ -82,20 +120,24 @@ void log_file_proxy::create_link(const std::string& time_ms)
     remove(file_name_with_dir_.c_str());
     if (symlink(log_file.c_str(), file_name_with_dir_.c_str()) == -1)
     {
-        printf("写日志时，创建链接文件失败:%s\n", file_name_with_dir_.c_str());
+        printf("创建日志链接文件失败, 线程id:%lu, 错误原因:%s, 日志文件:%s, 链接文件:%s\n", pthread_self(), strerror(errno), log_file.c_str(), file_name_with_dir_.c_str());
+    }
+    else
+    {
+        printf("创建日志链接文件成功, 线程id:%lu, 日志文件:%s, 链接文件:%s\n", pthread_self(), log_file.c_str(), file_name_with_dir_.c_str());
     }
 }
 
 void log_file_proxy::write_file(const std::string& log)
 {
     int size = write(fd_, log.c_str(), log.length());
-    if (size != -1)
+    if (size > 0)
     {
         curr_file_size_ += size;
     }
     else
     {
-        printf("写日志到文件失败:%s\n", log.c_str());
+        printf("写日志到文件失败, 线程id:%lu, 错误原因:%s, 日志内容:%s\n", pthread_self(), strerror(errno), log.c_str());
     }
 }
 
--- test/main.cpp
+++ test/main.cpp
@@ -6,13 +6,14 @@
 void log_test()
 {
     clock_timer timer;
-    for (int i = 1; i <= 1000000; ++i)
+    for (int i = 1; i <= 10000000; ++i)
     {
         /* log_file_debug("12345678901234567890123456789012345678901234567890123456789012345678901234567890name:{}--age:{}", "chxuan", 3.124); */
         log_file_debug("hello world");
+        log_file_warn("hello world");
     }
 
-    std::cout << "线程:" << std::this_thread::get_id() << ", 耗时:" << timer.elapsed() << "ms" << std::endl;
+    std::cout << "线程:" << pthread_self() << ", 耗时:" << timer.elapsed() << "ms" << std::endl;
 }
 
 int main()
@@ -20,24 +21,27 @@ int main()
     init_easylog("./log", log_level::all, 100 * 1024 * 1024);
 
     /* log_test(); */
-    /* std::thread t1([]{ log_test(); }); */
-    /* std::thread t2([]{ log_test(); }); */
+    std::thread t1([]{ log_test(); });
+    std::thread t2([]{ log_test(); });
 
-    /* t1.join(); */
-    /* t2.join(); */
+    t1.join();
+    t2.join();
 
-    clock_timer timer;
-    for (int i = 1; i <= 1000000; ++i)
-    {
-        /* log_file_debug("12345678901234567890123456789012345678901234567890123456789012345678901234567890name:{}--age:{}", "chxuan", 3.124); */
-        log_file_debug("hello world");
-    }
+    /* clock_timer timer; */
+    /* for (int i = 1; i <= 1000000; ++i) */
+    /* { */
+    /*     log_file_debug("12345678901234567890123456789012345678901234567890123456789012345678901234567890name:{}--age:{}", "chxuan", 3.124); */
+    /*     /1* log_file_debug("hello world"); *1/ */
+    /* } */
 
-    std::cout << "线程:" << std::this_thread::get_id() << ", 耗时:" << timer.elapsed() << "ms" << std::endl;
+    /* std::cout << "线程:" << std::this_thread::get_id() << ", 耗时:" << timer.elapsed() << "ms" << std::endl; */
 
     /* std::cout << "线程:" << std::this_thread::get_id() << ", 耗时:" << timer.elapsed() << "ms" << std::endl; */
     /* std::string name = "Jack"; */
     /* int age = 20; */
+    /* log_file_debug("name:{}--age:{}", name, age); */
+    /* char* p = NULL; */
+    /* *p = 1; */
     /* log_file_debug("name:{}--age:{}", name, age); */
     /* log_file_debug("name:--age:"); */
     /* log_debug("name:{}--age:{}", name, age); */
